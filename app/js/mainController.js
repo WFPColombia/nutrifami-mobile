deac11eaebddf921b10195e63c1aed4469bf1146
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-07-31 20:39:24 -0500

    New. Login.

--- app/js/mainController.js
+++ app/js/mainController.js
@@ -1,22 +1,41 @@
 /*global angular*/
 
-var dependencies = ['mobile-angular-ui', 'ngRoute'];
+dependencies = ['Authentication', 'ngRoute', 'ngCookies', 'ngAudio', 'bsLoadingOverlay', 'mobile-angular-ui', 'ngAnimate'];
 
-/*try {
-    angular.module("ngMockE2E");
-    dependencies.push("ngMockE2E");
-} catch (err) {
-}*/
+var nutrifamiLogin = angular.module('Authentication', []);
+var nutrifamiMobile = angular.module('NutrifamiMobile', dependencies);
 
-var AsisconApp = angular.module('Asiscon', dependencies);
-
-AsisconApp.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+nutrifamiMobile.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
         'use strict';
 
         $locationProvider.html5Mode();
 
-        $routeProvider.when('/', {
-            templateUrl: 'view/home.html'
-            , controller: 'HomeController'
+        $routeProvider.when('/login', {
+            controller: 'LoginController',
+            templateUrl: 'views/login.html',
+            hideMenus: true
         });
+
+        $routeProvider.when('/', {
+            controller: 'HomeController',
+            templateUrl: 'views/home.html'
+        });
+
+        $routeProvider.otherwise({redirectTo: '/'});
     }]);
+
+nutrifamiMobile.run(['$rootScope', '$location', '$cookieStore', 'bsLoadingOverlayService',
+    function ($rootScope, $location, $cookieStore, bsLoadingOverlayService) {
+        // keep user logged in after page refresh
+        $rootScope.globals = $cookieStore.get('globals') || {};
+
+        nutrifami.getSessionId();
+
+        $rootScope.$on('$locationChangeStart', function (event, next, current) {
+            // redirect to login page if not logged in
+            if ($location.path() !== '/login' && !$rootScope.globals.currentUser) {
+                $location.path('/login');
+            }
+        });
+    }
+]);
