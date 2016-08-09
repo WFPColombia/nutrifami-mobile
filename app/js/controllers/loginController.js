342af503df1724b5fe38cfa1ec4295e567c89ef8
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-08-09 04:18:35 -0500

    Fix. Pantalla completa al abrir teclado

--- app/js/controllers/loginController.js
+++ app/js/controllers/loginController.js
@@ -1,20 +1,29 @@
 nutrifamiLogin.controller('LoginController', ['$scope', '$location', 'AuthenticationService',
     function ($scope, $location, AuthenticationService) {
         'use strict';
-        // reset login status
-        AuthenticationService.ClearCredentials();
-        localStorage.clear();
 
-        $scope.login = function () {
-            $scope.dataLoading = true;
-            AuthenticationService.Login($scope.username, $scope.password, function (response) {
-                if (response.success) {
-                    AuthenticationService.SetCredentials($scope.username, $scope.password, response.message);
-                    $location.path('/');
-                } else {
-                    $scope.error = response.message;
-                    $scope.dataLoading = false;
-                }
-            });
-        };
+        document.addEventListener('deviceready', function () {
+            // reset login status
+
+            AndroidFullScreen.immersiveMode();
+            AuthenticationService.ClearCredentials();
+            localStorage.clear();
+
+            $scope.login = function () {
+                $scope.dataLoading = true;
+                AuthenticationService.Login($scope.username, $scope.password, function (response) {
+                    if (response.success) {
+                        AuthenticationService.SetCredentials($scope.username, $scope.password, response.message);
+                        $location.path('/');
+                    } else {
+                        $scope.error = response.message;
+                        $scope.dataLoading = false;
+                    }
+                });
+            };
+
+
+        }, false);
+
+
     }]);
