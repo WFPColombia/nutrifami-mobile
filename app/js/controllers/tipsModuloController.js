04d5dbe1f07e1b1be7084631b5f425f6545761f3
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-09-21 07:42:04 -0500

    IONIC. Add. Sección de tips por modulos

--- app/js/controllers/tipsModuloController.js
+++ app/js/controllers/tipsModuloController.js
@@ -7,16 +7,48 @@ nutrifamiMobile.controller('TipsModuloController', function ($ionicPlatform, $sc
     $scope.usuarioActivo = UsuarioService.getUsuarioActivo();
 
 
-    $scope.groups = [];
-    for (var i = 0; i < 10; i++) {
-        $scope.groups[i] = {
-            name: i,
-            items: []
-        };
-        for (var j = 0; j < 3; j++) {
-            $scope.groups[i].items.push(i + '-' + j);
+    $scope.groups = [
+        {
+            name: "La Alimentación",
+            items: [
+                'Prevenga deficiencias de nutrientes consumiendo una alimentación variada y colorida.',
+                'Modere el consumo de alimentos con grasas para tener una buena salud. Prefiera aceites vegetales.',
+                'Prevenga deficiencias de nutrientes con el consumo de una alimentación variada y colorida.',
+                'Consuma alimentos de todos los grupos, prefiera los de cosecha ya que serán más frescos, económicos y disponibles.'
+            ]
+        },
+        {
+            name: "Los Alimentos",
+            items: [
+                'Prevenga deficiencias de nutrientes consumiendo una alimentación variada y colorida.',
+                'Modere el consumo de alimentos con grasas para tener una buena salud. Prefiera aceites vegetales.',
+                'Prevenga deficiencias de nutrientes con el consumo de una alimentación variada y colorida.',
+                'Consuma alimentos de todos los grupos, prefiera los de cosecha ya que serán más frescos, económicos y disponibles.'
+            ]
+        },
+        {
+            name: "El plato saludable",
+            items: [
+                'Prevenga deficiencias de nutrientes consumiendo una alimentación variada y colorida.',
+                'Modere el consumo de alimentos con grasas para tener una buena salud. Prefiera aceites vegetales.',
+                'Prevenga deficiencias de nutrientes con el consumo de una alimentación variada y colorida.',
+                'Consuma alimentos de todos los grupos, prefiera los de cosecha ya que serán más frescos, económicos y disponibles.'
+            ]
+        },
+        {
+            name: "Los colores de los alimentos",
+            items: [
+                'Prevenga deficiencias de nutrientes consumiendo una alimentación variada y colorida.',
+                'Modere el consumo de alimentos con grasas para tener una buena salud. Prefiera aceites vegetales.',
+                'Prevenga deficiencias de nutrientes con el consumo de una alimentación variada y colorida.',
+                'Consuma alimentos de todos los grupos, prefiera los de cosecha ya que serán más frescos, económicos y disponibles.'
+            ]
         }
-    }
+    ];
+    
+    $scope.clickTip = function (){
+        console.log("Click Tip");
+    };
 
     /*
      * if given group is the selected group, deselect it
