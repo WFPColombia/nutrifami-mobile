557890d40611cd394a4eede73d994739611fdc4f
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2016-10-10 22:34:31 -0500

    IONIC. Fix Agregar familiar en perfil

--- app/js/controllers/perfilController.js
+++ app/js/controllers/perfilController.js
@@ -1,109 +1,110 @@
 /*global angular*/
-nutrifamiMobile.controller('PerfilController', function ($ionicPlatform, $scope, $rootScope, PerfilService, UsuarioService) {
+nutrifamiMobile.controller('PerfilController', function ($ionicPlatform, $scope, $rootScope, $ionicPopup, PerfilService, UsuarioService) {
     'use strict';
-    /* BEGIN CORDOVA FILES
-     $ionicPlatform.ready(function () {
-     END CORDOVA FILES */
-    
-    /* Cargamos la información del local storage*/
-    $scope.usuarioActivo = UsuarioService.getUsuarioActivo();
-    $scope.usuarioFamilia = JSON.parse(localStorage.getItem('usuarioFamilia'));
+    $ionicPlatform.ready(function () {
 
-    console.log($scope.usuarioActivo);
-    console.log($scope.usuarioFamilia);
+        /* Cargamos la información del local storage*/
+        $scope.usuarioActivo = UsuarioService.getUsuarioActivo();
+        $scope.usuarioFamilia = UsuarioService.getUsuarioFamilia();
 
-    /* Creamos un arreglo para mostrar los miembros de la familia de forma dinamica */
-    $scope.usuarioFamilia.miembrosPorRango = [
-        {
-            rango: '0 a 2 años',
-            cantidad: parseInt($scope.usuarioActivo.rango_0a2),
-            rango_alias: '0a2'
-        },
-        {
-            rango: '2 a 5 años',
-            cantidad: parseInt($scope.usuarioActivo.rango_2a5),
-            rango_alias: '2a5'
-        },
-        {
-            rango: '6 a 17 años',
-            cantidad: parseInt($scope.usuarioActivo.rango_6a17),
-            rango_alias: '6a17'
-        },
-        {
-            rango: '18 a 60 años',
-            cantidad: parseInt($scope.usuarioActivo.rango_18a60),
-            rango_alias: '18a60'
-        },
-        {
-            rango: '60 0 más años',
-            cantidad: parseInt($scope.usuarioActivo.rango_60mas),
-            rango_alias: '60mas'
-        }
-    ];
+        console.log($scope.usuarioActivo);
+        console.log($scope.usuarioFamilia);
 
-    $scope.usuarioFamilia.totalMiembrosPorInscribir = 0;
-    for (var i in $scope.usuarioFamilia.miembrosPorRango) {
-        $scope.usuarioFamilia.totalMiembrosPorInscribir = $scope.usuarioFamilia.totalMiembrosPorInscribir + $scope.usuarioFamilia.miembrosPorRango[i].cantidad;
-    }
-
-    $scope.familiar = {};
-    $scope.familiar.FAM_PER_NOMBRE = '';
-    $scope.familiar.FAM_PER_APELLIDO = '';
-    $scope.familiar.parentescos = {
-        availableOptions: [
-            {id: 'hijo', name: 'Hijo'},
-            {id: 'conyuge', name: 'Conyuge'},
-            {id: 'padre', name: 'Padre'},
-            {id: 'otros', name: 'Otros'}
-        ]
-    };
-
-    $scope.usuarioFamilia.totalMiembrosInscritos = $scope.usuarioFamilia.length;
-
-    $scope.abrirModalEditarFamiliar = function (index) {
-        $scope.indexFamiliar = index;
-        $rootScope.Ui.turnOn('editarFamiliar');
-    };
-
-    $scope.agregarFamiliar = function () {
-        var index = $scope.indexFamiliar;
-        console.log(index);
-        var familiar = $scope.familiar;
-        familiar.FAM_PER_PARENTESCO = familiar.parentescos.selectedOption.id;
-        familiar.FAM_PER_JEFE = $scope.usuarioActivo.id;
-        familiar.FAM_PER_CODIGO = $scope.usuarioActivo.login_codigo;
-        familiar.documento_jefe = $scope.usuarioActivo.login_documento;
-        familiar.FAM_PER_BIRTHDATE = "0";
-
-        /* If para verificar si es usuario nuevo o miembro de la familia */
-        if (typeof $scope.miembro === 'undefined') {
-            familiar.rango = false;
-            familiar.cantidad = 0;
-        } else {
-            familiar.rango = $scope.miembro.rango_alias;
-            familiar.cantidad = $scope.miembro.cantidad - 1;
-        }
-
-        delete familiar["parentescos"];
-
-        PerfilService.agregarFamiliar(familiar, function (response) {
-            if (response.success) {
-                if (familiar.rango !== false) {
-                    $scope.usuarioFamilia.miembrosPorRango[index].cantidad--;
-                    $scope.usuarioActivo['rango_' + familiar.rango] = familiar.cantidad;
-                }
-
-                $scope.usuarioFamilia.push(familiar);
-
-                localStorage.setItem("usuarioActivo", JSON.stringify($scope.usuarioActivo));
-                localStorage.setItem("usuarioFamilia", JSON.stringify($scope.usuarioFamilia));
-                $rootScope.Ui.turnOff('editarFamiliar');
-            } else {
-                console.log(response.message);
+        /* Creamos un arreglo para mostrar los miembros de la familia de forma dinamica */
+        $scope.usuarioFamilia.miembrosPorRango = [
+            {
+                rango: '0 a 2 años',
+                cantidad: parseInt($scope.usuarioActivo.rango_0a2),
+                rango_alias: '0a2'
+            },
+            {
+                rango: '2 a 5 años',
+                cantidad: parseInt($scope.usuarioActivo.rango_2a5),
+                rango_alias: '2a5'
+            },
+            {
+                rango: '6 a 17 años',
+                cantidad: parseInt($scope.usuarioActivo.rango_6a17),
+                rango_alias: '6a17'
+            },
+            {
+                rango: '18 a 60 años',
+                cantidad: parseInt($scope.usuarioActivo.rango_18a60),
+                rango_alias: '18a60'
+            },
+            {
+                rango: '60 0 más años',
+                cantidad: parseInt($scope.usuarioActivo.rango_60mas),
+                rango_alias: '60mas'
             }
-        });
-    };
-    /* BEGIN CORDOVA FILES
-     });
-     END CORDOVA FILES */
+        ];
+
+        $scope.usuarioFamilia.totalMiembrosPorInscribir = 0;
+        for (var i in $scope.usuarioFamilia.miembrosPorRango) {
+            $scope.usuarioFamilia.totalMiembrosPorInscribir = $scope.usuarioFamilia.totalMiembrosPorInscribir + $scope.usuarioFamilia.miembrosPorRango[i].cantidad;
+        }
+
+        $scope.familiar = {};
+        $scope.familiar.FAM_PER_NOMBRE = '';
+        $scope.familiar.FAM_PER_APELLIDO = '';
+        $scope.familiar.parentescos = {
+            availableOptions: [
+                {id: 'hijo', name: 'Hijo'},
+                {id: 'conyuge', name: 'Conyuge'},
+                {id: 'padre', name: 'Padre'},
+                {id: 'otros', name: 'Otros'}
+            ]
+        };
+
+        $scope.usuarioFamilia.totalMiembrosInscritos = $scope.usuarioFamilia.length;
+
+        $scope.abrirModalEditarFamiliar = function (index) {
+            console.log(index);
+            $scope.indexFamiliar = index;
+            var popUpAgregarFamiliar = $ionicPopup.show({
+                templateUrl: 'views/template/agregarFamiliar.tpl.html',
+                scope: $scope,
+                cssClass: 'agregar-familiar'
+            });
+        };
+
+        $scope.agregarFamiliar = function () {
+            var index = $scope.indexFamiliar;
+            var familiar = $scope.familiar;
+            familiar.FAM_PER_PARENTESCO = 'otros';
+            familiar.FAM_PER_JEFE = $scope.usuarioActivo.id;
+            familiar.FAM_PER_CODIGO = $scope.usuarioActivo.login_codigo;
+            familiar.documento_jefe = $scope.usuarioActivo.login_documento;
+            familiar.FAM_PER_BIRTHDATE = "0";
+
+            console.log($scope.miembro);
+            /* If para verificar si es usuario nuevo o miembro de la familia */
+            if (typeof $scope.miembro === 'undefined') {
+                familiar.rango = false;
+                familiar.cantidad = 0;
+            } else {
+                familiar.rango = $scope.miembro.rango_alias;
+                familiar.cantidad = $scope.miembro.cantidad - 1;
+            }
+
+            delete familiar["parentescos"];
+
+            /*PerfilService.agregarFamiliar(familiar, function (response) {
+             if (response.success) {
+             if (familiar.rango !== false) {
+             $scope.usuarioFamilia.miembrosPorRango[index].cantidad--;
+             $scope.usuarioActivo['rango_' + familiar.rango] = familiar.cantidad;
+             }
+             
+             $scope.usuarioFamilia.push(familiar);
+             
+             localStorage.setItem("usuarioActivo", JSON.stringify($scope.usuarioActivo));
+             localStorage.setItem("usuarioFamilia", JSON.stringify($scope.usuarioFamilia));
+             //$rootScope.Ui.turnOff('editarFamiliar');
+             } else {
+             console.log(response.message);
+             }
+             });*/
+        };
+    });
 });
